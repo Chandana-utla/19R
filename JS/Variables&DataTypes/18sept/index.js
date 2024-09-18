c853d3f03a0b29cbe026c2115b924edfe666717b
Chandana-utla <127076522+Chandana-utla@users.noreply.github.com>
2024-09-18 18:49:34 +0530

    Add files via upload

---
+++ JS/Variables&DataTypes/18sept/index.js
@@ -0,0 +1,11 @@
+var age=22;
+var dept="CSE";
+var student="yes";
+console.log(age);
+console.log(dept);
+console.log(student);
+age=25;
+console.log(age);
+console.log(batch);
+batch='19R';
+
---
+++ JS/Variables&DataTypes/18sept/task.js
@@ -0,0 +1,17 @@
+var Dad;
+var Mom;
+var me;
+var sister;
+var fam={parent1:'father',parent2:'mother',child1:'me',child2:'sister',details:{members:4,parents:2,children:2}};
+Dad={name:'Venu',age:45,occupation:'Gold-smith'};
+Mom=[{name:'indira',age:42,children:2},{child1:'chandu',child2:'chikki'}];
+me={name:'Chandu',age:22,student:'yes'};
+sister={name:'omsree',student:'yes',dept:'ECE'};
+console.log(Dad);
+console.log(Mom);
+console.log(me);
+console.log(sister);
+console.log(Dad.name);
+console.log(Mom[0]['age']);
+console.log(fam.details.members)
+
